e5bc942f12a0cb5ad78040723ce35dd7f105f951
Masterchef365 <duncan.freeman1@gmail.com>
2022-04-12 20:36:49 -0700

    trees

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct TriangleApp {
 
 impl App for TriangleApp {
     fn init(ctx: &mut Context, platform: &mut Platform, _: ()) -> Result<Self> {
-        let (vertices, indices) = sierpiński_triangle_verts(11, 0.05, [1.; 3]);
+        let (vertices, indices) = sierpiński_triangle_verts(10, 0.05, [1.; 3]);
         Ok(Self {
             verts: ctx.vertices(&vertices, false)?,
             indices: ctx.indices(&indices, false)?,
@@ -65,14 +65,14 @@ fn sierpiński_triangle(iters: usize) -> impl Iterator<Item = (f32, f32)> {
         if rem > 0 {
             match chr {
                 'x' => stack.extend([
+                    (rem - 1, 'y', -1), //
+                    (rem - 1, 'x', -1), //
                     (rem - 1, 'y', 1),
-                    (rem - 1, 'x', 1),
-                    (rem - 1, 'y', 0),
                 ]),
                 'y' => stack.extend([
+                    (rem - 1, 'x', 1),  //
+                    (rem - 1, 'y', 1), //
                     (rem - 1, 'x', -1),
-                    (rem - 1, 'y', -1),
-                    (rem - 1, 'x', 0),
                 ]),
                 _ => unreachable!(),
             }
@@ -96,12 +96,16 @@ fn add_tup((a, b): (f32, f32), (c, d): (f32, f32)) -> (f32, f32) {
 fn sierpiński_triangle_verts(
     iters: usize,
     scale: f32,
-    color: [f32; 3],
+    _color: [f32; 3],
 ) -> (Vec<Vertex>, Vec<u32>) {
     let vertices: Vec<Vertex> = sierpiński_triangle(iters)
-        .map(|(x, y)| Vertex {
-            pos: [x * scale, 0., y * scale],
-            color,
+        .enumerate()
+        .map(|(idx, (x, y))| {
+            let f = idx as f32 / 10_000.;
+            Vertex {
+                pos: [x * scale, 0., y * scale],
+                color: [f.cos().abs(), f.sin().abs(), (f * 3.).sin().abs()],
+            }
         })
         .collect();
 
