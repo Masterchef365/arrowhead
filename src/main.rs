d508e989b8708b4e0e16de31ba9035099a961713
Masterchef365 <duncan.freeman1@gmail.com>
2022-04-12 21:18:10 -0700

    Attempt at 3d

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use idek::{prelude::*, IndexBuffer, MultiPlatformCamera};
+use idek::{
+    nalgebra::{Quaternion, UnitQuaternion, UnitVector3, Vector3},
+    prelude::*,
+    IndexBuffer, MultiPlatformCamera,
+};
 
 fn main() -> Result<()> {
     launch::<_, TriangleApp>(Settings::default().vr_if_any_args())
@@ -13,7 +17,8 @@ struct TriangleApp {
 
 impl App for TriangleApp {
     fn init(ctx: &mut Context, platform: &mut Platform, _: ()) -> Result<Self> {
-        let (vertices, indices) = sierpiński_triangle_verts(11, 0.05, [1.; 3]);
+        let (vertices, indices) = sierpiński_triangle_verts(12, 0.05, [1.; 3]);
+        dbg!(vertices.len());
         Ok(Self {
             verts: ctx.vertices(&vertices, false)?,
             indices: ctx.indices(&indices, false)?,
@@ -46,16 +51,25 @@ impl App for TriangleApp {
     }
 }
 
-fn sierpiński_triangle(iters: usize) -> impl Iterator<Item = (f32, f32)> {
-    let directions: Vec<(f32, f32)> = (0..6)
-        .map(|i| i as f32 * std::f32::consts::PI / 3. as f32)
-        .map(|a| (a.cos(), a.sin()))
-        .collect();
+fn sierpiński_triangle(iters: usize) -> impl Iterator<Item = Vector3<f32>> {
+    let mut pos = Vector3::zeros();
+    let mut dir = UnitQuaternion::identity();
+    let step = Vector3::new(1., 0., 0.);
 
-    let mut pos = (0., 0.);
-    let mut dir: i32 = 0;
+    let rot_angle = std::f32::consts::PI / 3.;
+    let star = |a: f32| {
+        UnitQuaternion::from_axis_angle(
+            &UnitVector3::new_normalize(Vector3::new(0., a.cos(), a.sin())),
+            rot_angle,
+        )
+    };
 
-    let mut stack = vec![(iters, 'y', 0)];
+    let star_angle = std::f32::consts::TAU / 3.;
+    let a = star(0.);
+    let b = star(1. * star_angle);
+    let c = star(2. * star_angle);
+
+    let mut stack = vec![(iters, 'y', 'a')];
 
     let next_vert = move || {
         let ret_pos = pos;
@@ -63,24 +77,36 @@ fn sierpiński_triangle(iters: usize) -> impl Iterator<Item = (f32, f32)> {
         let (rem, chr, rot) = stack.pop()?;
 
         if rem > 0 {
+            let rem = rem - 1;
             match chr {
                 'x' => stack.extend([
-                    (rem - 1, 'y', 1),
-                    (rem - 1, 'x', 1),
-                    (rem - 1, 'y', 0),
+                    (rem, 'y', 'a'),
+                    (rem, 'z', 'a'),
+                    (rem, 'y', '_'),
                 ]),
                 'y' => stack.extend([
-                    (rem - 1, 'x', -1),
-                    (rem - 1, 'y', -1),
-                    (rem - 1, 'x', 0),
+                    (rem, 'z', 'b'),
+                    (rem, 'x', 'b'),
+                    (rem, 'z', '_'),
+                ]),
+                'z' => stack.extend([
+                    (rem, 'x', 'c'),
+                    (rem, 'y', 'c'),
+                    (rem, 'x', '_'),
                 ]),
                 _ => unreachable!(),
             }
         }
 
-        dir = (dir + rot).rem_euclid(directions.len() as i32);
-        if rot != 0 {
-            pos = add_tup(pos, directions[dir as usize]);
+        dir = match rot {
+            'a' => dir * a,
+            'b' => dir * b,
+            'c' => dir * c,
+            _ => dir,
+        };
+
+        if rot != '_' {
+            pos += dir * step;
         }
 
         Some(ret_pos)
@@ -89,8 +115,8 @@ fn sierpiński_triangle(iters: usize) -> impl Iterator<Item = (f32, f32)> {
     std::iter::once(pos).chain(std::iter::from_fn(next_vert))
 }
 
-fn add_tup((a, b): (f32, f32), (c, d): (f32, f32)) -> (f32, f32) {
-    (a + c, b + d)
+fn add_tup((a, b, c): (f32, f32, f32), (d, e, f): (f32, f32, f32)) -> (f32, f32, f32) {
+    (a + d, b + e, c + f)
 }
 
 fn sierpiński_triangle_verts(
@@ -99,8 +125,8 @@ fn sierpiński_triangle_verts(
     color: [f32; 3],
 ) -> (Vec<Vertex>, Vec<u32>) {
     let vertices: Vec<Vertex> = sierpiński_triangle(iters)
-        .map(|(x, y)| Vertex {
-            pos: [x * scale, 0., y * scale],
+        .map(|v| Vertex {
+            pos: (v * scale).into(),
             color,
         })
         .collect();
